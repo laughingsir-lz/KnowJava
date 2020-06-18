199805a53be2076c907a11b07ebdf3b57d479b48
laughingsir-lz <1399519087@qq.com>
2020-06-18 12:09:50 +0800

    2016/6/17  拓展练习

---
+++ src/com/lz/generalpractice/expandpractice_1/Employee.java
@@ -0,0 +1,64 @@
+package com.lz.generalpractice.expandpractice_1;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Employee 类
+ */
+public class Employee {
+    private String name;
+    private int age;
+    private BigDecimal salary;
+    private Date hireDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = new BigDecimal(salary);
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) throws ParseException {
+        //创建格式化对象sdf
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        //设置格式
+        String patten = "yyyy-MM-dd";
+        sdf.applyPattern(patten);
+        Date date = sdf.parse(hireDate);
+        this.hireDate = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", hireDate=" + hireDate.toLocaleString() +
+                '}';
+    }
+}
---
+++ src/com/lz/generalpractice/expandpractice_1/EmployeeDemo.java
@@ -0,0 +1,49 @@
+package com.lz.generalpractice.expandpractice_1;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/**
+ * 拓展练习一
+ */
+public class EmployeeDemo {
+    public static void main(String[] args) throws ParseException {
+        String string = "name=小红&age=17&salary=8000.00&hireDate=1991-07-03;" +
+                "name=小明&age=23&salary=10000.00&hireDate=1992-12-11;" +
+                "name=小白&age=18&salary=12000.00&hireDate=1996-01-09;";
+        //设置时间格式
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        BigDecimal sum = new BigDecimal("0.00"); //总金额
+        int count = 0;//计数器
+        String[] strings = string.split(";");//分割数组
+        Employee[] employees = new Employee[strings.length];//员工信息数组
+        for (int i = 0; i < strings.length; i++) {
+            String[] s = strings[i].split("[&]");
+            for (int j = 0; j < s.length; j++) {
+                //组装员工信息
+                Employee employee = new Employee();
+                employee.setName(s[0].substring(5));
+                employee.setAge(Integer.parseInt(s[1].substring(4)));
+                employee.setSalary(s[2].substring(7));
+                employee.setHireDate(s[3].substring(9));
+                employees[i] = employee;
+            }
+            //判断入职日期
+            Date bt = sdf.parse(s[3].substring(9));
+            Date et = sdf.parse("1996-01-01");
+            if (bt.before(et)) {
+                //表示bt小于et
+                BigDecimal bigDecimal = new BigDecimal(s[2].substring(7));
+                sum = sum.add(bigDecimal);
+                count++;
+            }
+            //员工信息
+            System.out.println(employees[i]);
+        }
+        //输出平均值
+        BigDecimal cBD = new BigDecimal(count);
+        System.out.println("在1996年前入职的员工有：" + count + " 位；\n"
+                + "平均工资为：" + sum.divide(cBD, 2, RoundingMode.HALF_UP));
+    }
+}
